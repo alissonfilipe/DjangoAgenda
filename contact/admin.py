b528dcc1f78d1fbe47de387a2f22d579aa65c747
Alisson Filipe <alissonsantos4@estudante.ifto.edu.br>
2024-06-27 17:23:58 -0300

    atualização do trabalho

--- contact/admin.py
+++ contact/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from contact import models  
+from . import models
 
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id','first_name', 'last_name', 'phone',
+    ordering = '-id',
+    # -id representa ordenar do jeito decrescente
+    #list_filter = 'created_date'
+    search_fields = 'id','first_name','last_name',
+    list_per_page = 10 # o que é pra ser mostrado por página
+    list_max_show_all = 200 # o máximo que é para ser mostrado
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone', # editable e display links não podem ter os mesmos campos
